44561833961c05d32066cef9f5c6f9dd8f555e29
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-02-04 17:00:49 -0700

    working on some animations for homepage and battle

--- react-client/src/pages/Home.js
+++ react-client/src/pages/Home.js
@@ -1,29 +1,39 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Wrapper from "../components/Wrapper";
-import { SectionRow , Col } from "../components/Grid";
+import { SectionRow, Col } from "../components/Grid";
+import { useSpring, animated } from "react-spring";
 
 const Home = () => {
-  const [ background, setBackground ] = useState({});
+  const props = useSpring({
+    to: async (next, cancel) => {
+      await next({ opacity: 1, marginTop: "-30px", duration: "3000" });
+      await next({ opacity: 3, marginTop: "800px", duration: "3000" });
+      await next({ opacity: 5, marginTop: "500px", duration: "3000" });
+    },
+    from: { opacity: 0, marginTop: "50px" }
+  });
+  const [background, setBackground] = useState({});
   useEffect(() => {
     const bg_collection = [
-      '/img/battle-bg1.gif',
-      '/img/battle-bg2.webp',
-      '/img/battle-bg3.webp',
-      '/img/battle-bg4.webp',
-      '/img/battle-bg5.webp',
-      '/img/choose-hero-bg1.gif',
-      '/img/choose-hero-bg2.gif',
-      '/img/choose-hero-bg3.webp',
-      '/img/choose-hero-bg4.webp',
-      '/img/choose-hero-bg5.webp',
+      "/img/battle-bg1.gif",
+      "/img/battle-bg2.webp",
+      "/img/battle-bg3.webp",
+      "/img/battle-bg4.webp",
+      "/img/battle-bg5.webp",
+      "/img/choose-hero-bg1.gif",
+      "/img/choose-hero-bg2.gif",
+      "/img/choose-hero-bg3.webp",
+      "/img/choose-hero-bg4.webp",
+      "/img/choose-hero-bg5.webp"
     ];
 
-    let bg_url = bg_collection[Math.floor(Math.random()*bg_collection.length)];
+    let bg_url =
+      bg_collection[Math.floor(Math.random() * bg_collection.length)];
     let bg_style = {
       backgroundImage: `url('${bg_url}')`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover'
-    }
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover"
+    };
 
     setBackground(bg_style);
   }, []);
@@ -32,10 +42,11 @@ const Home = () => {
     <Wrapper className="App" id="main-container" style={background}>
       <SectionRow id="main-section">
         <Col size="lg-12">
+          <animated.h1 style={props}>Battle Heroes</animated.h1>
         </Col>
       </SectionRow>
     </Wrapper>
   );
 };
 
-export default Home; 
+export default Home;
