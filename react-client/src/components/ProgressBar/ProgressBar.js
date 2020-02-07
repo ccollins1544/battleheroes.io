5f7e0c897e377b28b46de6dc068988685f3cfd8a
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-02-07 16:29:24 -0700

    new progress bar with hooks decreases health by 20 percent right now

--- react-client/src/components/ProgressBar/ProgressBar.js
+++ react-client/src/components/ProgressBar/ProgressBar.js
@@ -1,14 +1,37 @@
-import React from "react";
-import { ProgressBar } from "react-bootstrap";
+import React, { useState } from "react";
+const Range = props => {
+  return (
+    // render current the filled range of progress bar along its width
+    <div className="range" style={{ width: `${props.percentRange}%` }} />
+  );
+};
 
-class ProgressBarPage extends React.Component {
-  render() {
-    return (
-      <div className="hello">
-        <ProgressBar animated now={60} />
+const ProgressBar = props => {
+  return (
+    <div className="progress-bar">
+      {/*render available progress bar’s limit*/}
+      <Range percentRange={props.percentRange} />
+    </div>
+  );
+};
+
+export const ProgressBarContainer = () => {
+  const [percentRange, setProgress] = useState(100);
+
+  return (
+    <div className="container">
+      {/*pass the percentageRange state to other components*/}
+      <ProgressBar percentRange={percentRange} />
+      <div className="toggle-buttons">
+        {/* call setProgress func on button click and bind the callback*/}
+        {/* depending on the percentageRange condition to decrease /*/}
+        {/* increase in 20% range and reset the progress bar status*/}
+        <button
+          onClick={() => setProgress(percentRange > 0 ? percentRange - 20 : 0)}
+        >
+          Attack
+        </button>
       </div>
-    );
-  }
-}
-
-export default ProgressBarPage;
+    </div>
+  );
+};
