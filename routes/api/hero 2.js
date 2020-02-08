ff97b7931a1a177222495059a76eea5fb4930484
Tyler Webb <tylweb0704@gmail.com>
2020-02-08 15:52:09 -0700

    deleted duplicate files

--- routes/api/hero 2.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const heroController = require("../../controllers/heroController");
-
-// Matches with "/api/hero/:hero_id"
-router.route("/:hero_id")
-  .get(heroController.findById);
-
-module.exports = router;
--- routes/api/sendEmail 2.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const sendEmailController = require("../../controllers/sendEmailController");
-
-// Matches with "/api/sendemail/"
-router.route("/")
-  .post(sendEmailController.sendMessage);
-
-module.exports = router;
